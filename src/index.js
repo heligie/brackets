6a2cb11894f6fa16d7aeb6baa2db0119070e6afa
heligie <heligiette@gmail.com>
2024-08-05 00:48:58 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = new Map(bracketsConfig);
+  let brackets = [];
+
+  for (const bracket of str) {
+    bracket === config.get(brackets[brackets.length - 1]) ?
+      brackets.pop() :
+      brackets.push(bracket);
+  }
+
+  return !brackets.length;
 }
